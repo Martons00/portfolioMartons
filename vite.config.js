5c88a2774ceb2ebd04ab1a07ae05e9b5b748b54c
Martons00 <raffaelemartone34@gmail.com>
2025-12-03 09:27:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/portfolioMartons",
-  server: { host: true }
-})
-
+  plugins: [react()],                    
+  base: "/portfolioMartons",             
+  server: { host: true },                
+  build: {
+    chunkSizeWarningLimit: 1200,         
+    rollupOptions: {
+      output: {
+        manualChunks: {                  
+          vendor: ['react', 'react-dom'],
+          three: ['three'], 
+          icons: ['react-icons/si'],
+        }
+      }
+    },
+  },
+});
